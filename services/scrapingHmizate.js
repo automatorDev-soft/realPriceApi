c049d03d51afa6bb3315ed52eea9f53261baff93
KhalidDev2004 <youftank@gmail.com>
2024-05-31 20:15:38 +0000

    add scrapingHmizate.js

---
+++ services/scrapingHmizate.js
@@ -0,0 +1,37 @@
+import puppeteer from 'puppeteer';
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: true });
+    const page = await browser.newPage();
+    await page.goto('https://hmizate.ma/', { waitUntil: 'domcontentloaded' });
+
+    await page.waitForSelector('.tvproduct-wrapper');
+
+    const products = await page.evaluate(() => {
+        let products = [];
+        let elements = document.querySelectorAll('.tvproduct-wrapper');
+
+        elements.forEach(element => {
+            let img = element.querySelector('.thumbnail img')?.getAttribute('src') || '';
+            let name = element.querySelector('.tvproduct-name')?.textContent.trim() || '';
+            let price = element.querySelector('.price')?.textContent || '';
+            let description = element.querySelector('.product-description')?.textContent || '';
+            let stars = element.querySelector('.tvall-product-star-icon')?.getAttribute('src') || '';
+
+            products.push({
+                img: img,
+                name: name,
+                price: price,
+                currency: "MAD",
+                description: description,
+                stars: stars,
+                source: "Hmizate"
+            });
+        });
+
+        return products;
+    });
+
+    console.log(products);
+    await browser.close();
+})();
